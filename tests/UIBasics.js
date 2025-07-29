312d9303b4f77315872e6dee7bb84329625ea9e4
Juan Carlos Suárez <suarez.jcarlos@gmail.com>
2025-07-28 21:43:07 -0600

    Add declaracionFixtures.js for testing purposes

--- tests/UIBasics.js
+++ tests/UIBasics.js
@@ -6,7 +6,7 @@ const {expect} = require('@playwright/test');
 
 
 
-test.only('Prueba falso login ', async({browser}) =>{
+test('Prueba falso login ', async({browser}) =>{
 
     const context  = await browser.newContext()
     const page = await context.newPage();
@@ -18,6 +18,36 @@ test.only('Prueba falso login ', async({browser}) =>{
     console.log(await page.locator("[style*='block']").textContent());
     // Verificar que el texto esperado esté presente
     await expect(page.locator("[style*='block']")).toHaveText('Incorrect username/password.');
+
+});
+//Prueba cuando existen varios elementos con el mismo locator
+//En este caso, se usa el locator como una variable para evitar duplicar código
+test('Prueba login ', async({browser}) =>{
+
+    const context  = await browser.newContext()
+    const page = await context.newPage();
+
+    //usando el locator como una variable
+    const locatorUsername = page.locator('input#username.form-control');
+    const locatorPassword = page.locator('input#password.form-control');
+    const locatorSignInButton = page.locator('input#signInBtn.btn.btn-info.btn-md');
+    const learningSelector = page.locator('text=learning'); // Selector para el texto "learning"
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    //SELECTORES 
+    await locatorUsername.fill('');
+    await locatorUsername.fill('rahulshettyacademy');
+    await locatorPassword.fill('');                     
+    await locatorPassword.fill('learning');
+    await locatorSignInButton.click();
+    
+    // Este selector encuentra varios elementos pero solo necesitamos el que contiene "iphone" 
+    const getiPhoneText = page.locator('.card-body .card-title a');
+
+    
+    console.log(await getiPhoneText.nth(0).textContent());
+
+   await expect(getiPhoneText).toContainText('iphone');
 
 });
 
@@ -32,4 +62,23 @@ test('Prueba de pagina', async({page}) =>{
     await expect(page).toHaveTitle('Google'); // Verifica que el título de la página sea 'Google'
 
 });
+
+
+
+//Haz una prueba que se conecte a google, escriba Mexico en el campo de búsqueda, y verifique que el primer resultado contenga la palabra "Mexico"
+test('Prueba de busqueda en Google', async({page}) =>{
+
+    await page.goto('https://google.com.mx');
+    const fillbox = page.locator('.gLFyf#APjFqb');
+    await fillbox.fill('México'); // Escribe 'Mexico' en el campo de búsqueda
+    await page.keyboard.press('Enter'); // Presiona Enter para buscar
+    await page.waitForTimeout(2000); // Espera un momento para que los resultados se carguen
+
+    // Verifica que el primer resultado contenga la palabra "Mexico"
+    const firstResult = await page.locator('h3').first().textContent();
+    console.log(firstResult);
+    expect(firstResult).toContain('México'); // Verifica que el primer resultado contenga 'Mexico'
+
+});
+
 
